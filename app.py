86cf19b6b105ba2c7c2f0567f0e47f99deab2a41
johnyu1234 <johnyu@hotmail.my>
2020-12-19 02:26:09 +0800

    added channel token

--- app.py
+++ app.py
@@ -51,7 +51,6 @@ if channel_access_token is None:
 line_bot_api = LineBotApi(channel_access_token)
 parser = WebhookParser(channel_secret)
 
-
 @app.route("/callback", methods=["POST"])
 def callback():
     signature = request.headers["X-Line-Signature"]
